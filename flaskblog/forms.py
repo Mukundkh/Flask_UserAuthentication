02d615674f71959e0947a522016d091edc5453eb
Mukund Khandelwal <khandelwalmukund23@gmail.com>
2021-12-10 00:48:36 +0530

    update

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from flask_login import current_user
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
@@ -18,8 +19,6 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user:
             raise ValidationError('The email is already taken!!')
-
-    
 
 class LoginForm(FlaskForm):
     email = StringField('Email',
@@ -43,3 +42,24 @@ class ResetPasswordForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password',validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Reset password')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Enter your name',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email Address',
+                        validators=[DataRequired(), Email()])
+    
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('The username is already taken!!')
+    
+    def validate_username(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('The email is already taken!!')
